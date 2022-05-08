54783484c6ce86780228ad26f85408a4175f3f3b
Pureshich <stremskijkirill@gmail.com>
2022-05-08 21:41:44 +0400

    added Work6

---
+++ basic-learn/src/test/java/ru/nshi/learn/Work6/Color.java
@@ -0,0 +1,47 @@
+package ru.nshi.learn.Work6;
+
+public class Color
+{
+    public int r,g,b;
+
+    public Color(int valueR,int valueG, int valueB)
+    {
+        if(valueR >= 0 && valueR <= 255 && valueG >= 0 && valueG <= 255 && valueB >= 0 && valueB <= 255)
+        {
+            this.r=valueR;
+            this.g=valueG;
+            this.b=valueB;
+        }
+        else
+        {
+            throw new IllegalArgumentException();
+        }
+    }
+    public void setRGB(int valueR,int valueG,int valueB)
+    {
+        if(valueR >= 0 && valueR <= 255)
+        {
+            this.r=valueR;
+        }
+        else
+        {
+            throw new IllegalArgumentException("значение "+valueR+" не входит в диапазон 0-255");
+        }
+        if(valueG >= 0 && valueG <= 255)
+        {
+            this.g = valueG;
+        }
+        else
+        {
+            throw new IllegalArgumentException("значение "+valueG+" не входит в диапазон 0-255");
+        }
+        if(valueB >= 0 && valueB <= 255)
+        {
+            this.b=valueB;
+        }
+        else
+        {
+            throw new IllegalArgumentException("значение "+valueB+" не входит в диапазон 0-255");
+        }
+    }
+}
